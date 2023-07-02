7f7761adde4f8edb319022a6f8e3ffdc42f45fca
Giorgi Tamarashvili <46628664+giorgi01@users.noreply.github.com>
2023-07-02 18:24:28 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
-from lib import QuadraticEquationSolver
-from pytest import approx, raises
+import pytest
+from calculator import Calculator
 
-def test_p1_0_0():
-    assert 0 == 0
+def test_add():
+    calc = Calculator()
+    assert calc.add(2, 3) == 5
+    assert calc.add(5, -1) == 4
+
+def test_subtract():
+    calc = Calculator()
+    assert calc.subtract(5, 3) == 2
+    assert calc.subtract(10, -2) == 12
+
+def test_multiply():
+    calc = Calculator()
+    assert calc.multiply(2, 3) == 6
+    assert calc.multiply(5, 0) == 0
+
+def test_divide():
+    calc = Calculator()
+    assert calc.divide(10, 2) == 5
+    assert calc.divide(20, -4) == -5
+
+def test_divide_by_zero():
+    calc = Calculator()
+    with pytest.raises(ValueError):
+        calc.divide(10, 0)
