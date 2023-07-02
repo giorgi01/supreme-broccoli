93a6746f775ad47a726cf28f4e876934138d6d68
Giorgi Tamarashvili <46628664+giorgi01@users.noreply.github.com>
2023-07-02 18:21:18 +0400

    Update lib.py

--- lib.py
+++ lib.py
@@ -3,17 +3,3 @@ class QuadraticEquationSolver:
         self.a = a
         self.b = b
         self.c = c
-
-    def solve(self):
-        d = self.b**2 - 4 * self.a * self.c
-
-        if d <= 0:
-            return []
-
-        if d == 0:
-            return [-self.b / (2 * self.a)]
-
-        return [
-            (-self.b + d**0.5) / (2 * self.a),
-            (-self.b - d**0.5) / (2 * self.a),
-        ]
